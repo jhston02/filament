753160b6c23077c37575c2ee326c2ce7b3f68dfc
jhston02 <coleslaw.john@gmail.com>
2023-11-01 19:11:32 -0700

    format

--- lib/common/common.ml
+++ lib/common/common.ml
@@ -5,40 +5,37 @@ module Isbn : sig
 end = struct
   type t = string [@@deriving eq]
 
-  let integer_to_digit_list integer = 
+  let integer_to_digit_list integer =
     let rec integer_to_digit_list_impl integer agg =
-      if integer < 10 then
-        (integer, agg)
-      else
-        integer_to_digit_list_impl (integer / 10) ((integer mod 10) :: agg)
+      if integer < 10 then (integer, agg)
+      else integer_to_digit_list_impl (integer / 10) ((integer mod 10) :: agg)
     in
     integer_to_digit_list_impl integer []
 
   let check_isbn_sum isbn_list =
-    let (check, list) = isbn_list in
-    let (agg,_) = List.fold_left
-               (fun (agg, multiple) value -> 
-                        let agg = agg + (value * multiple) in
-                        let multiple = if multiple = 1 then 3 else 1 in
-                        (agg, multiple)) (0, 1) list in
+    let check, list = isbn_list in
+    let agg, _ =
+      List.fold_left
+        (fun (agg, multiple) value ->
+          let agg = agg + (value * multiple) in
+          let multiple = if multiple = 1 then 3 else 1 in
+          (agg, multiple))
+        (0, 1) list
+    in
     let check_value = 10 - (agg mod 10) in
-    if check_value = 10 then
-      check = 0
-    else
-      check = check_value
-  
+    if check_value = 10 then check = 0 else check = check_value
+
   let check_isbn isbn =
     let open Containers.Option.Infix in
-      int_of_string_opt isbn >|= integer_to_digit_list >|= check_isbn_sum
-    
+    int_of_string_opt isbn >|= integer_to_digit_list >|= check_isbn_sum
 
   let create isbn =
-     if String.length isbn = 13 then
+    if String.length isbn = 13 then
       match check_isbn isbn with
       | Some false -> None
       | Some true -> Some isbn
       | None -> None
-     else None
+    else None
 end
 
 module Pages : sig
--- test/common/test_common.ml
+++ test/common/test_common.ml
@@ -5,7 +5,7 @@ let negative =
     QCheck.(neg_int)
     (fun l -> Pages.create l = None)
 
-let positive = 
+let positive =
   QCheck.Test.make ~count:1000 ~name:"pages_positive_returns_some"
     QCheck.(pos_int)
     (fun l -> Option.is_some (Pages.create l))
