cd38fddbe7aa5d56769160a7d2d7b5eab03c8bba
jhston02 <coleslaw.john@gmail.com>
2023-11-05 00:35:51 -0700

    fmt

--- lib/common/common.ml
+++ lib/common/common.ml
@@ -5,28 +5,30 @@ module Isbn : sig
 end = struct
   type t = string [@@deriving eq]
 
-  let string_to_digit_list integer_list = 
+  let string_to_digit_list integer_list =
     let open Containers in
-    (String.fold_right (fun c agg -> ((Char.to_int c)-48) :: agg) integer_list)
-    []
+    (String.fold_right (fun c agg -> (Char.to_int c - 48) :: agg) integer_list)
+      []
 
-  let check_range_values = 
-    List.for_all (fun x -> x < 10 && x >= 0)
+  let check_range_values = List.for_all (fun x -> x < 10 && x >= 0)
 
   let check_isbn_sum isbn_list =
-    let agg = List.mapi (fun i digit -> if (i mod 2) = 0 then digit else 3 * digit) isbn_list |> List.fold_left (+) 0 in
-    (agg mod 10) = 0
-  
+    let agg =
+      List.mapi
+        (fun i digit -> if i mod 2 = 0 then digit else 3 * digit)
+        isbn_list
+      |> List.fold_left ( + ) 0
+    in
+    agg mod 10 = 0
+
   let check_isbn isbn =
-    let digit_list = isbn |> string_to_digit_list in 
+    let digit_list = isbn |> string_to_digit_list in
     check_range_values digit_list && check_isbn_sum digit_list
-    
+
   let create isbn =
-     if String.length isbn = 13 then
-      match check_isbn isbn with
-      | false -> None
-      | true -> Some isbn
-     else None
+    if String.length isbn = 13 then
+      match check_isbn isbn with false -> None | true -> Some isbn
+    else None
 end
 
 module Pages : sig
