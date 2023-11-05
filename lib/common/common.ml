5175b0c4079c35b86974da0a2f30d3ff2ae17f08
jhston02 <coleslaw.john@gmail.com>
2023-11-05 00:34:56 -0700

    Flesh out unit tests and fix issues

--- lib/common/common.ml
+++ lib/common/common.ml
@@ -5,37 +5,28 @@ module Isbn : sig
 end = struct
   type t = string [@@deriving eq]
 
-  let integer_to_digit_list integer =
-    let rec integer_to_digit_list_impl integer agg =
-      if integer < 10 then (integer, agg)
-      else integer_to_digit_list_impl (integer / 10) ((integer mod 10) :: agg)
-    in
-    integer_to_digit_list_impl integer []
+  let string_to_digit_list integer_list = 
+    let open Containers in
+    (String.fold_right (fun c agg -> ((Char.to_int c)-48) :: agg) integer_list)
+    []
+
+  let check_range_values = 
+    List.for_all (fun x -> x < 10 && x >= 0)
 
   let check_isbn_sum isbn_list =
-    let check, list = isbn_list in
-    let agg, _ =
-      List.fold_left
-        (fun (agg, multiple) value ->
-          let agg = agg + (value * multiple) in
-          let multiple = if multiple = 1 then 3 else 1 in
-          (agg, multiple))
-        (0, 1) list
-    in
-    let check_value = 10 - (agg mod 10) in
-    if check_value = 10 then check = 0 else check = check_value
-
+    let agg = List.mapi (fun i digit -> if (i mod 2) = 0 then digit else 3 * digit) isbn_list |> List.fold_left (+) 0 in
+    (agg mod 10) = 0
+  
   let check_isbn isbn =
-    let open Containers.Option.Infix in
-    int_of_string_opt isbn >|= integer_to_digit_list >|= check_isbn_sum
-
+    let digit_list = isbn |> string_to_digit_list in 
+    check_range_values digit_list && check_isbn_sum digit_list
+    
   let create isbn =
-    if String.length isbn = 13 then
+     if String.length isbn = 13 then
       match check_isbn isbn with
-      | Some false -> None
-      | Some true -> Some isbn
-      | None -> None
-    else None
+      | false -> None
+      | true -> Some isbn
+     else None
 end
 
 module Pages : sig
@@ -45,7 +36,7 @@ module Pages : sig
 end = struct
   type t = int [@@deriving eq]
 
-  let create page = if page < 0 then Some page else None
+  let create page = if page > 0 then Some page else None
 end
 
 type book_id = Book_id of string [@@deriving eq]
--- test/common/test_common.ml
+++ test/common/test_common.ml
@@ -1,11 +1,47 @@
 open Filament.Common
+open QCheck2.Gen
 
-let negative =
-  QCheck.Test.make ~count:1000 ~name:"pages_negative_returns_none"
-    QCheck.(neg_int)
+let negative_pages =
+  QCheck2.Test.make ~count:1000 ~name:"pages_negative_returns_none"
+   ((-10000) -- (-1))
     (fun l -> Pages.create l = None)
 
-let positive =
-  QCheck.Test.make ~count:1000 ~name:"pages_positive_returns_some"
-    QCheck.(pos_int)
-    (fun l -> Option.is_some (Pages.create l))
+let positive_pages = 
+  QCheck2.Test.make ~count:1000 ~name:"pages_positive_returns_some"
+    (1 -- 10000)
+    (fun l ->  Option.is_some (Pages.create l))
+
+(* Function to generate ISBN-13 digits excluding the checksum *)
+let gen_isbn13_digits =
+  int_range 0 9
+  |> list_repeat 12
+
+(* Calculate the checksum digit for ISBN-13 *)
+let calculate_checksum_digit (digits : int list) : int =
+  let weighted_sum = List.mapi (fun i digit -> if (i mod 2) = 0 then  digit else 3 * digit) digits |> List.fold_left (+) 0 in
+  let checksum = (10 - (weighted_sum mod 10)) in
+  if checksum = 10 then 0 else checksum
+
+(* Generator for ISBN-13 numbers *)
+let gen_isbn13  =
+  let digits_gen = gen_isbn13_digits in
+  digits_gen
+  |> map (fun digits ->
+      let checksum_digit = calculate_checksum_digit digits in
+      let digits_str = List.map string_of_int (digits @ [checksum_digit]) in
+      String.concat "" digits_str)
+
+let valid_isbn =
+  QCheck2.Test.make ~count:1000 ~name:"valid_isbn_returns_some"
+    gen_isbn13
+    (fun isbn -> Option.is_some (Isbn.create isbn))
+
+
+let () =
+  let suite = 
+    List.map (QCheck_alcotest.to_alcotest ~verbose:true)
+      [ positive_pages ; negative_pages ; valid_isbn ]
+  in
+  Alcotest.run "my test" [
+    "suite", suite
+  ]
