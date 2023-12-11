7af71ea6d283af7665a797b5cdd8faa200b089d5
jhston02 <coleslaw.john@gmail.com>
2023-12-10 17:27:40 -0800

    more work on bookshelf

--- lib/domain/bookshelf.ml
+++ lib/domain/bookshelf.ml
@@ -1,13 +1,57 @@
 open Common
 
-type bookshelf = {
+type t = {
   id : bookshelf_id;
   owner_id : owner_id;
   books : book_id list;
   name : string;
+  deleted : bool;
 }
 
-type t = Deleted of bookshelf | In_use of bookshelf
-
-type e = Bookshelf_created of bookshelf | Bookshelf_deleted of bookshelf_event
+type e = Bookshelf_created of t | Bookshelf_deleted of bookshelf_event | Bookshelf_renamed of bookshelf_event | Book_added of bookshelf_event * book_id | Book_removed of bookshelf_event * book_id
 and bookshelf_event = { owner_id : owner_id; bookshelf_id : bookshelf_id }
+
+let empty = {
+  id = Bookshelf_id "";
+  owner_id = Owner_id "";
+  books = [];
+  name = "";
+  deleted = false;
+}
+
+let create id owner_id name =
+  let bookshelf = { id; owner_id; books=[]; name ; deleted=false} in
+  Ok ([Bookshelf_created bookshelf])
+
+let delete (bookshelf: t) =
+  match bookshelf.deleted with
+  | true -> Error "Bookshelf already deleted"
+  | false -> Ok ([Bookshelf_deleted { owner_id = bookshelf.owner_id; bookshelf_id = bookshelf.id }])
+
+let rename (bookshelf: t) name =
+  match bookshelf.deleted with
+  | true -> Error "Bookshelf already deleted"
+  | false -> Ok ([Bookshelf_renamed { owner_id = bookshelf.owner_id; bookshelf_id = bookshelf.id }])
+
+let add_book (bookshelf: t) book_id =
+  match bookshelf.deleted with
+  | true -> Error "Bookshelf already deleted"
+  | false when List.mem book_id bookshelf.books -> Error "Book already in bookshelf"
+  | false -> Ok ([Book_added ({ owner_id = bookshelf.owner_id; bookshelf_id = bookshelf.id }, book_id)])
+
+let remove_book (bookshelf: t) book_id =
+  match bookshelf.deleted with
+  | true -> Error "Bookshelf already deleted"
+  | false when not (List.mem book_id bookshelf.books) -> Error "Book not in bookshelf"
+  | false ->  Ok ([Book_removed ({ owner_id = bookshelf.owner_id; bookshelf_id = bookshelf.id }, book_id)])
+
+let evolve bookshelf event =
+  let evolve_impl bookshelf event =
+    match event with
+    | Bookshelf_created bookshelf -> bookshelf
+    | Bookshelf_deleted _ -> { bookshelf with deleted = true }
+    | Bookshelf_renamed _ -> bookshelf
+    | Book_added (_, book_id) -> { bookshelf with books = book_id :: bookshelf.books }
+    | Book_removed (_, book_id) -> { bookshelf with books = List.filter (fun id -> id <> book_id) bookshelf.books }
+  in
+  List.fold_left evolve_impl bookshelf event
--- lib/domain/bookshelf.mli
+++ lib/domain/bookshelf.mli
@@ -3,12 +3,18 @@ open Common
 type t
 type e
 
-val create : bookshelf_id -> owner_id -> (e list, string) result
+val create : bookshelf_id -> owner_id -> string -> (e list, string) result
 val add_book : t -> book_id -> (e list, string) result
 val remove_book : t -> book_id -> (e list, string) result
 val rename : t -> string -> (e list, string) result
 val evolve : t -> e list -> t
 
 module Private : sig
-  type t
+  val create_bookshelf_created_event : book_id -> owner_id -> Isbn.t -> Pages.t -> e
+  val create_bookshelf_deleted_event : book_id -> owner_id -> e
+  val create_book_added_event : book_id -> owner_id -> e
+  val create_book_removed_event : book_id -> owner_id -> e
+  val create_bookshelf_renamed_event : book_id -> owner_id -> e
+
+  val create_bookshelf : bookshelf_id -> owner_id -> string -> bool -> t
 end
